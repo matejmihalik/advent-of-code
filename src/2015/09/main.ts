544770b05f888ac677ebfe74a0032e8fb3f0bee5
Matej Mihalik <matejm@protonmail.com>
2023-06-04 14:02:54 +0200

    Add solution for 2015/09

---
+++ src/2015/09/main.ts
@@ -0,0 +1,83 @@
+// --- Day 9: All in a Single Night ---
+// https://adventofcode.com/2015/day/9
+
+import { InputReader } from '#src/InputReader.ts';
+
+const inputReader = new InputReader(import.meta.url);
+const routes = inputReader.readAsLines();
+
+type DistanceMap = Map<string, Map<string, number>>;
+
+function setDistanceToMap(
+    map: DistanceMap,
+    startLocation: string,
+    targetLocation: string,
+    distance: number,
+): void {
+    let startLocationMap = map.get(startLocation);
+
+    if (!startLocationMap) {
+        startLocationMap = new Map();
+        map.set(startLocation, startLocationMap);
+    }
+
+    startLocationMap.set(targetLocation, distance);
+}
+
+const DISTANCE_MAP = routes.reduce<DistanceMap>((distanceMap, route) => {
+    const routeMatch = route.match(
+        /^(?<startLocation>\w+) to (?<endLocation>\w+) = (?<distance>\d+)$/,
+    );
+
+    if (!routeMatch?.groups) {
+        return distanceMap;
+    }
+
+    const { startLocation, endLocation, distance } = routeMatch.groups;
+
+    setDistanceToMap(distanceMap, startLocation, endLocation, Number(distance));
+    setDistanceToMap(distanceMap, endLocation, startLocation, Number(distance));
+
+    return distanceMap;
+}, new Map());
+
+const ALL_LOCATIONS = Array.from(DISTANCE_MAP.keys());
+
+function getBestDistanceBetweenLocations(
+    fitnessFunction: (distance?: number) => number,
+    locations: string[],
+    startingLocation?: string,
+): number | undefined {
+    return locations.reduce<number | undefined>(
+        (bestDistance, immediateLocation, immediateLocationIndex) => {
+            const otherTargetLocations = [...locations];
+            otherTargetLocations.splice(immediateLocationIndex, 1);
+
+            const immediateDistance = startingLocation
+                ? DISTANCE_MAP.get(startingLocation)?.get(immediateLocation) ?? 0
+                : 0;
+
+            const remainingBestDistance =
+                getBestDistanceBetweenLocations(
+                    fitnessFunction,
+                    otherTargetLocations,
+                    immediateLocation,
+                ) ?? 0;
+
+            const distance = immediateDistance + remainingBestDistance;
+
+            return fitnessFunction(distance) > fitnessFunction(bestDistance)
+                ? distance
+                : bestDistance;
+        },
+        undefined,
+    );
+}
+
+export function partOne() {
+    return getBestDistanceBetweenLocations((distance) => -(distance ?? Infinity), ALL_LOCATIONS);
+}
+
+export function partTwo() {
+    return getBestDistanceBetweenLocations((distance) => distance ?? 0, ALL_LOCATIONS);
+}
